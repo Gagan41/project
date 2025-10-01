87d4f4a2716e1233e22cbf9d2cd41dbc8dcbce85
Gagan41 <gagant.p100@gmail.com>
2025-10-01 20:00:29 +0530

    photo final

--- src/components/CourseBenefitsAndFAQ.tsx
+++ src/components/CourseBenefitsAndFAQ.tsx
@@ -89,7 +89,7 @@ export default function CourseBenefitsAndFAQ() {
               className="flex justify-center"
             >
               <Image
-                src="/photo.jpg"
+                src="/ash.jpg"
                 alt="About Me"
                 width={400}
                 height={400}
